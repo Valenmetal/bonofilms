a7287c013e6087a66b1fbc45598ddedacd18012e
Valentino Geronzi <valentinogeronzi@hotmail.com>
2025-07-22 12:15:05 -0300

    Stars visual bug fixed

--- src/components/Estrella.jsx
+++ src/components/Estrella.jsx
@@ -33,7 +33,7 @@ export const Estrella = () => {
    }, []);
 
    return (
-      <div className="logo-container">
+      <div className="estrellita-container">
          <svg
             className="estrellita"
             xmlns="http://www.w3.org/2000/svg"
